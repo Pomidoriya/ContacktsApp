004deea52bc19309f7dab0975747694612e7a7c3
Игорь Сердюк <igorserd2909@gmail.com>
2021-09-20 20:59:56 +0700

    Добавлен проект логики

--- ContacktsAppUI/Form1.cs
+++ ContacktsAppUI/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ContacktsApp;
 namespace ContacktsAppUI
 {
     public partial class Form1 : Form
@@ -15,6 +15,11 @@ namespace ContacktsAppUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
