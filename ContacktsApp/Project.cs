28ad69f66ffe606057dd115d72b052474ebf1ec2
Игорь Сердюк <igorserd2909@gmail.com>
2021-10-03 01:42:04 +0700

    1) Разработана логика приложения, добавленны классы: Contact, PhoneNumber, Project, ProjectManager 2) Добавлена библиотека Newtonsoft JSON.NET, добавлен класс сериализации

---
+++ ContacktsApp/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContacktsApp
+{
+    /// <summary>
+    /// Класс, содержащий лист всех контактов.
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// Лист, который хранит в себе список контактов.
+        /// </summary>
+        public List<Contact> contactsList = new List<Contact>();
+    }
+}
